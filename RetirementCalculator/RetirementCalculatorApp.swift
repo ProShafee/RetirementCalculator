dcda8b16e948afab4e50b961fcf64972d458b025
Shafee Rehman <shafee.rehman@xplortechnologies.com>
2023-08-19 11:42:54 +1200

    Initial Commit

---
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementCalculatorApp.swift
+//  RetirementCalculator
+//
+//  Created by Shafee Rehman on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
