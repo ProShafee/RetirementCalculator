5302f5ad3dca9232ab3187296a74163c965b7beb
Shafee Rehman <shafee.rehman@xplortechnologies.com>
2023-08-19 12:01:58 +1200

    Appcenter added

--- RetirementCalculator/RetirementCalculatorApp.swift
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -6,9 +6,17 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @main
 struct RetirementCalculatorApp: App {
+    
+    init() {
+        AppCenter.start(withAppSecret: "4668d64b-5945-4f00-b09c-e1d61b5668ae", services: [Analytics.self, Crashes.self])
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
